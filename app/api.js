dbe8a0c23bc13062930568593443ddd810a0265d
Grant J. Butler <butler.grant.j@gmail.com>
2014-04-25 17:42:05 -0400

    Added API endpoints.

---
+++ app/api.js
@@ -0,0 +1,27 @@
+module.exports = function(app) {
+	var mongo = app.get('mongo'),
+		tweetsCollection = mongo.collection('tweets'),
+		wordsCollection = mongo.collection('words');
+	
+	app.get('/api/tweets', function(req, res) {
+		tweetsCollection.find().toArray(function(err, tweets) {
+			if (err) {
+				res.json(500, { 'error': err });
+			}
+			else {
+				res.json({ 'tweets': tweets });
+			}
+		});
+	});
+	
+	app.get('/api/words', function(req, res) {
+		wordsCollection.find().toArray(function(err, words) {
+			if (err) {
+				res.json(500, { 'error': err });
+			}
+			else {
+				res.json({ 'words': words });
+			}
+		});
+	});
+};
