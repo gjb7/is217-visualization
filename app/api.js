6dfa7c2801c71fb21d122120791fac2fe5bb4e06
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-13 15:35:46 -0400

    Updated to implement searching of tweets.

--- app/api.js
+++ app/api.js
@@ -3,64 +3,84 @@ module.exports = function(app) {
 		tweetsCollection = mongo.collection('tweets'),
 		wordsCollection = mongo.collection('words');
 	
-	app.get('/api/tweets', function(req, res) {
-		tweetsCollection.find().toArray(function(err, tweets) {
-			if (err) {
-				res.json(500, { 'error': err });
-			}
-			else {
-				res.json({ 'tweets': tweets });
-			}
-		});
-	});
-	
-	app.get('/api/words', function(req, res) {
-		wordsCollection.find().toArray(function(err, words) {
-			if (err) {
-				res.json(500, { 'error': err });
-			}
-			else {
-				res.json({ 'words': words });
-			}
-		});
-	});
-	
-	app.get('/api/words/weights', function(req, res) {
-		wordsCollection.find({ 'average occurrences' : { $gt : '0.75' } }).toArray(function(err, words) {
-			if (err) {
-				res.json(500, { 'error': err });
-			}
-			else {
-				var normalizedWords = {};
-				
-				words.forEach(function(value) {
-					if (value.words in normalizedWords) {
-						normalizedWords[value.words] += Number(value['average occurrences']);
-					}
-					else {
-						normalizedWords[value.words] = Number(value['average occurrences']);
-					}
-				});
-				
-				var weights = [];
-				
-				for (var word in normalizedWords) {
-					weights.push([ word, normalizedWords[word] ]);
+	tweetsCollection.ensureIndex({ text: 'text' }, function(err) {
+		if (err) {
+			throw err;
+		}
+		
+		app.get('/api/tweets', function(req, res) {
+			tweetsCollection.find().toArray(function(err, tweets) {
+				if (err) {
+					res.json(500, { 'error': err });
 				}
-				
-				weights.sort(function(a, b) {
-					if (a[1] < b[1]) {
-						return 1;
-					}
-					else if (a[1] > b[1]) {
-						return -1;
+				else {
+					res.json({ 'tweets': tweets });
+				}
+			});
+		});
+		
+		app.get('/api/words', function(req, res) {
+			wordsCollection.find().toArray(function(err, words) {
+				if (err) {
+					res.json(500, { 'error': err });
+				}
+				else {
+					res.json({ 'words': words });
+				}
+			});
+		});
+		
+		app.get('/api/words/weights', function(req, res) {
+			wordsCollection.find({ 'average occurrences' : { $gt : '0.75' } }).toArray(function(err, words) {
+				if (err) {
+					res.json(500, { 'error': err });
+				}
+				else {
+					var normalizedWords = {};
+					
+					words.forEach(function(value) {
+						if (value.words in normalizedWords) {
+							normalizedWords[value.words] += Number(value['average occurrences']);
+						}
+						else {
+							normalizedWords[value.words] = Number(value['average occurrences']);
+						}
+					});
+					
+					var weights = [];
+					
+					for (var word in normalizedWords) {
+						weights.push([ word, normalizedWords[word] ]);
 					}
 					
-					return 0;
-				});
-				
-				res.json(weights);
-			}
+					weights.sort(function(a, b) {
+						if (a[1] < b[1]) {
+							return 1;
+						}
+						else if (a[1] > b[1]) {
+							return -1;
+						}
+						
+						return 0;
+					});
+					
+					res.json(weights);
+				}
+			});
+		});
+		
+		app.get('/api/tweet/:word', function(req, res) {
+			mongo.command({ text: 'tweets', search: req.params.word }, function(err, object) {
+				if (err) {
+					res.json(500, { 'error': err });
+				}
+				else {
+					var results = object.results;
+					var tweet = results[Math.floor(Math.random() * results.length)].obj;
+					
+					res.json(tweet);
+				}
+			});
 		});
 	});
 };
