0533d993acc3db99583898777f0ff33c61e9d772
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-13 14:53:53 -0400

    Updated api to filter words under a certain number of average occurrences.

--- app/api.js
+++ app/api.js
@@ -26,17 +26,27 @@ module.exports = function(app) {
 	});
 	
 	app.get('/api/words/weights', function(req, res) {
-		wordsCollection.find().toArray(function(err, words) {
+		wordsCollection.find({ 'average occurrences' : { $gt : '0.75' } }).toArray(function(err, words) {
 			if (err) {
 				res.json(500, { 'error': err });
 			}
 			else {
-				var weights = words.map(function(val) {
-					return [
-						val.words,
-						val['average occurrences']
-					];
+				var normalizedWords = {};
+				
+				words.forEach(function(value) {
+					if (value.words in normalizedWords) {
+						normalizedWords[value.words] += Number(value['average occurrences']);
+					}
+					else {
+						normalizedWords[value.words] = Number(value['average occurrences']);
+					}
 				});
+				
+				var weights = [];
+				
+				for (var word in normalizedWords) {
+					weights.push([ word, normalizedWords[word] ]);
+				}
 				
 				res.json(weights);
 			}
