c0da116e4e390e64c0fc50077e824ede82273130
Alex Koenig <alex@ubuntu-vm-server-host.(none)>
2014-05-13 15:00:30 -0400

    Playing around with some settings on sizes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){ 
 	$.getJSON( "api/words/weights", function( data ) {
   		WordCloud($('#canvas')[0], {
-  			list: data
+  			list: data,
+  			weightFactor: 1.5,
+  			minSize: 8
+  			
   			});
 });
 });
